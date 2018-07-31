db0680901474373b94a0fa2960cfd41a64231227
PDC4-Training.PDC4 <PDC4-Training.PDC4@P4A-508SD52.dir.svc.accenture.com>
2018-07-31 12:22:52 +0530

    commit 3

--- MyFramework/src/test/java/com/accenture/cucumbertest/StepDefinition.java
+++ MyFramework/src/test/java/com/accenture/cucumbertest/StepDefinition.java
@@ -7,7 +7,7 @@ import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-
+//New code
 public class StepDefinition {
 	WebDriver d;
 	@Given("^I am on login page$")
